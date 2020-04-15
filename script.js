7b5f6588cdfe8a37d260ef55284cc1df6c392944
Rob <rob.w.ward@gmail.com>
2020-04-15 12:24:33 +0100

    m

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-function MenuFader(elem) {
-    var fadeStart = ($(window).height() * 0.2)
-        , elPosition = ($(elem).offset().top - $(window).scrollTop())
-        , elOpacity = (elPosition / fadeStart);
-    if (elOpacity > 1) {
-        $(elem).css("opacity", "1");
-    }
-    else {
-        $(elem).css("opacity", elOpacity);
-    };
-}
-
-$(".menuscroller").scroll(function () {
-    MenuFader(".menuitem");
+$(document).ready(function () {
+    $(window).on("scroll", function () {
+        var titleOpacity = 1 - ($(".menuscroller").scrollTop() / window.innerHeight) * 2;
+        $('.logocover').css('opacity', titleOpacity)
+    });
 });
